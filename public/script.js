cf9d4d6ceab8ec3bacf94631945a31aa6ee29301
terra-earth <zidanegtg000@gmail.com>
2023-03-20 07:30:54 +0700

    tugasmodul3

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 // api url
-const api_url = "https://api.thingspeak.com/channels/<ChannelID>/feeds.json?api_key=<ReadAPIKeys>&results=1";
+const api_url = "https://api.thingspeak.com/channels/2071399/feeds.json?api_key=6ADK6UUVCOQ7HSWR&results=1";
 // masukan url sesuai dengan format <ChannelID>, <ReadAPIKeys> yang ada di Thingspeak, tanpa menggunakan < >
 
 setInterval(() => {
@@ -10,5 +10,6 @@ setInterval(() => {
       var obj = JSON.parse(field);
       document.getElementById("field-1").innerHTML = obj.field1;
       document.getElementById("field-2").innerHTML = obj.field2;
+      document.getElementById("field-3").innerHTML = obj.field3;
     });
 }, 1000);
